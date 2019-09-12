e00290828590196357cd4b77e8e7c09c59a372f4
collaborative-bug-2010 <detrodoniv@yahoo.com>
2019-09-12 16:54:03 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -49,7 +49,7 @@ def checkout(array, coupons)
   hash_cart = consolidate_cart(array)
   applied_coupons = apply_coupons(hash_cart, coupons)
   discounts = apply_clearance(applied_coupons)
-  total = 0 
+  total = discounts.reduce
   discounts.each do |key,value| 
 binding.pry
 end
