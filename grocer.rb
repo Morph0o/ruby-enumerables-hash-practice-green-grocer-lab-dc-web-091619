366c38ee2dc857e40b7f7e2cb1e9777633351ced
collaborative-bug-2010 <detrodoniv@yahoo.com>
2019-09-12 16:44:34 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -50,7 +50,7 @@ def checkout(array, coupons)
   applied_coupons = apply_coupons(hash_cart, coupons)
   discounts = apply_clearance(applied_coupons)
   total = 0 
-  discounts.each do |item| 
+  discounts.each do |key,value| 
 binding.pry
 end
   
